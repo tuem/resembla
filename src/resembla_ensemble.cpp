c891b1e483d08ed372f1a993d874656002c44b54
tue <uemura.takashi@gmail.com>
2017-07-25 05:41:44 +0900

    improve score computation in ensemble method

--- src/resembla_ensemble.cpp
+++ src/resembla_ensemble.cpp
@@ -35,12 +35,17 @@ void ResemblaEnsemble::append(const std::shared_ptr<ResemblaInterface> resembla,
 
 std::vector<ResemblaEnsemble::response_type> ResemblaEnsemble::getSimilarTexts(const string_type& input, size_t max_response, double threshold)
 {
+    double total_weight = 0.;
+    for(auto p: resemblas){
+        total_weight += p.second;
+    }
+
     // find similar texts using all measures
     std::unordered_map<string_type, double> aggregated;
     for(auto p: resemblas){
         for(auto r: p.first->getSimilarTexts(input, max_response, threshold)){
             if(aggregated.find(r.text) == aggregated.end()){
-                aggregated[r.text] = 0L;
+                aggregated[r.text] = 0.;
             }
             aggregated[r.text] += p.second * r.score * r.score;
         }
@@ -49,7 +54,7 @@ std::vector<ResemblaEnsemble::response_type> ResemblaEnsemble::getSimilarTexts(c
     // sort combined result
     std::vector<response_type> response;
     for(auto r: aggregated){
-        response.push_back({r.first, measure_name, sqrt(r.second)});
+        response.push_back({r.first, measure_name, sqrt(r.second / total_weight)});
     }
     std::sort(std::begin(response), std::end(response));
 
