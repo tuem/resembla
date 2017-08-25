784904672c608f6e309f5b9d18149ab91b88bb14
tuem <uemura.takashi@gmail.com>
2017-08-25 21:47:59 +0900

    add simple tests for serialization and deserialization

---
+++ test/test_json.cpp
@@ -0,0 +1,81 @@
+/*
+Resembla: Word-based Japanese similar sentence search library
+https://github.com/tuem/resembla
+
+Copyright 2017 Takashi Uemura
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <string>
+#include <iostream>
+
+#include <Catch/catch.hpp>
+#include <json.hpp>
+
+#include "string_util.hpp"
+
+#include "measure/asis_sequence_builder.hpp"
+#include "measure/word_sequence_builder.hpp"
+#include "measure/keyword_match_preprocessor.hpp"
+#include "measure/weighted_sequence_serializer.hpp"
+
+using namespace resembla;
+using json = nlohmann::json;
+
+TEST_CASE( "serialize and deserialize output data of asis sequence builder", "[serialization]" ) {
+    init_locale();
+
+    AsIsSequenceBuilder<string_type>::output_type o0 = L"テキスト!";
+
+    json j0 = o0;
+    const std::string s = j0.dump();
+    CHECK(s == "[12486,12461,12473,12488,33]");
+
+    json j1 = json::parse(s);
+    AsIsSequenceBuilder<string_type>::output_type o1 = j1;
+    CHECK(o1== o0);
+}
+
+TEST_CASE( "serialize and deserialize output data of keyword match preprocessor", "[serialization]" ) {
+    init_locale();
+
+    KeywordMatchPreprocessor<string_type>::output_type o0 = {L"テキスト!", {L"キーワード0", L"キーワード1"}};
+
+    json j0 = o0;
+    const std::string s = j0.dump();
+    CHECK(s == "{\"keywords\":[\"キーワード0\",\"キーワード1\"],\"text\":\"テキスト!\"}");
+
+    json j1 = json::parse(s);
+    KeywordMatchPreprocessor<string_type>::output_type o1 = j1;
+    CHECK(o1.text == o0.text);
+    CHECK(o1.keywords == o0.keywords);
+}
+
+TEST_CASE( "serialize and deserialize output data of weighted romaji sequence builder", "[serialization]" ) {
+    init_locale();
+
+    WeightedSequenceBuilder<RomajiSequenceBuilder, RomajiMatchWeight>::output_type o0 = {{L'T', 0.3}, {L'e', 0.7}};
+
+    json j0 = o0;
+    const std::string s = j0.dump();
+    CHECK(s == "[{\"token\":\"T\",\"weight\":0.3},{\"token\":\"e\",\"weight\":0.7}]");
+
+    json j1 = json::parse(s);
+    WeightedSequenceBuilder<RomajiSequenceBuilder, RomajiMatchWeight>::output_type o1 = j1;
+    REQUIRE(o1.size() == o0.size());
+    for(size_t i = 0; i < o1.size(); ++i){
+        CHECK(o1[i].token == o1[i].token);
+        CHECK(o1[i].weight == o1[i].weight);
+    }
+}
