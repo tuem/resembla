a8bca2d791c7e96b788646964f5845f6ac7057a4
tuem <uemura.takashi@gmail.com>
2017-09-21 14:27:45 +0900

    use bped

--- src/basic_resembla.hpp
+++ src/basic_resembla.hpp
@@ -106,8 +106,7 @@ public:
             return {};
         }
         else if(simstring_result.size() > max_reranking_num){
-            //narrow_down_by_edit_distance(search_query, simstring_result, max_reranking_num);
-            narrow_down_by_unigram_intersection(search_query, simstring_result, max_reranking_num);
+            narrow_down_by_edit_distance(search_query, simstring_result, max_reranking_num);
         }
 
         std::vector<string_type> candidate_texts;
