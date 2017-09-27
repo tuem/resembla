002e0a0cd5edd49063f836654c37a6491183af1e
tuem <uemura.takashi@gmail.com>
2017-09-27 18:47:14 +0900

    move definitions

--- src/resembla_ensemble.hpp
+++ src/resembla_ensemble.hpp
@@ -20,31 +20,75 @@ limitations under the License.
 #ifndef RESEMBLA_RESEMBLA_ENSEMBLE_HPP
 #define RESEMBLA_RESEMBLA_ENSEMBLE_HPP
 
+#include <math.h>
 #include <string>
 #include <vector>
 #include <memory>
 #include <unordered_map>
+#include <algorithm>
 
 #include "resembla_interface.hpp"
 
 namespace resembla {
 
+template<typename Indexer, typename ScoreFunction>
 class ResemblaEnsemble: public ResemblaInterface
 {
 public:
-    ResemblaEnsemble(const std::string& measure_name, const size_t max_reranking_num = 0);
+    ResemblaEnsemble(const std::string& measure_name, const size_t max_candidate = 0):
+        measure_name(measure_name), max_candidate(max_candidate), total_weight(0.0) {}
 
-    void append(const std::shared_ptr<ResemblaInterface> resembla, const double weight = 1.0);
+    void append(const std::shared_ptr<ResemblaInterface> resembla, const double weight = 1.0)
+    {
+        resemblas.push_back(std::make_pair(resembla, weight));
+        total_weight += weight;
+    }
 
-    std::vector<output_type> find(const string_type& input, double threshold = 0.0, size_t max_response = 0) const;
+    std::vector<output_type> find(const string_type& query,
+            double threshold = 0.0, size_t max_response = 0) const
+    {
+        double t = max_candidate > 0 ? 0.0 : threshold;
+        size_t n = max_candidate > 0 ? max_candidate : max_response;
+
+        // find similar texts using all measures
+        std::unordered_map<string_type, double> aggregated;
+        for(auto p: resemblas){
+            for(auto r: p.first->find(query, t, n)){
+                if(aggregated.find(r.text) == std::end(aggregated)){
+                    aggregated[r.text] = 0.0;
+                }
+                aggregated.at(r.text) += p.second * r.score * r.score;
+            }
+        }
+
+        return eval(aggregated, threshold, max_response);
+    }
+
     std::vector<output_type> eval(const string_type& query, const std::vector<string_type>& targets,
-            double threshold = 0.0, size_t max_response = 0) const;
+            double threshold = 0.0, size_t max_response = 0) const
+    {
+        double t = max_candidate > 0 ? 0.0 : threshold;
+        size_t n = max_candidate > 0 ? max_candidate : max_response;
+
+        // calculate similarity using all measures
+        std::unordered_map<string_type, double> aggregated;
+        for(auto p: resemblas){
+            for(auto r: p.first->eval(query, targets, t, n)){
+                if(aggregated.find(r.text) == std::end(aggregated)){
+                    aggregated[r.text] = 0.0;
+                }
+                aggregated.at(r.text) += p.second * r.score * r.score;
+            }
+        }
+
+        return eval(aggregated, threshold, max_response);
+    }
 
 protected:
     // name to be used in response
     const std::string measure_name;
 
-    const size_t max_reranking_num;
+    const size_t max_candidate;
 
     double total_weight;
 
@@ -53,6 +97,23 @@ protected:
 
     std::vector<output_type> eval(const std::unordered_map<string_type, double>& aggregated,
             double threshold, size_t max_response) const;
+    {
+        // sort combined result
+        std::vector<output_type> response;
+        for(auto r: aggregated){
+            double score = sqrt(r.second / total_weight);
+            if(score >= threshold){
+                response.push_back({r.first, measure_name, score});
+            }
+        }
+        std::sort(std::begin(response), std::end(response));
+
+        // return at most max_response responses
+        if(max_response != 0 && response.size() > max_response){
+            response.erase(std::begin(response) + max_response, std::end(response));
+        }
+        return response;
+    }
 };
 
 }
