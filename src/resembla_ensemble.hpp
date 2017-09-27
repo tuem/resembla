ce03f352366042fdcbdac6a50206f94f38d90069
tuem <uemura.takashi@gmail.com>
2017-09-27 18:50:20 +0900

    add N-gram search and elimination

--- src/resembla_ensemble.hpp
+++ src/resembla_ensemble.hpp
@@ -27,7 +27,10 @@ limitations under the License.
 #include <unordered_map>
 #include <algorithm>
 
+#include <simstring/simstring.h>
+
 #include "resembla_interface.hpp"
+#include "eliminator.hpp"
 
 namespace resembla {
 
@@ -35,45 +38,51 @@ template<typename Indexer, typename ScoreFunction>
 class ResemblaEnsemble: public ResemblaInterface
 {
 public:
-    ResemblaEnsemble(const std::string& measure_name, const size_t max_candidate = 0):
-        measure_name(measure_name), max_candidate(max_candidate), total_weight(0.0) {}
-
-    void append(const std::shared_ptr<ResemblaInterface> resembla, const double weight = 1.0)
+    ResemblaEnsemble(const std::string& db_path, const std::string& index_path,
+            std::shared_ptr<Indexer> indexer, std::shared_ptr<ScoreFunction> score,
+            const std::string& measure_name, const size_t max_candidate = 0):
+        indexer(indexer), score(score),
+        measure_name(measure_name), max_candidate(max_candidate),
+        total_weight(0.0)
     {
-        resemblas.push_back(std::make_pair(resembla, weight));
+        load(db_path, index_path);
+    }
+
+    void append(const std::shared_ptr<ResemblaInterface> resembla, double weight = 1.0)
+    {
+        children.push_back(std::make_pair(resembla, weight));
         total_weight += weight;
     }
 
     std::vector<output_type> find(const string_type& query,
             double threshold = 0.0, size_t max_response = 0) const
     {
-        double t = max_candidate > 0 ? 0.0 : threshold;
-        size_t n = max_candidate > 0 ? max_candidate : max_response;
+        string_type search_query = indexer->index(query);
 
-        // find similar texts using all measures
-        std::unordered_map<string_type, double> aggregated;
-        for(auto p: resemblas){
-            for(auto r: p.first->find(query, t, n)){
-                if(aggregated.find(r.text) == std::end(aggregated)){
-                    aggregated[r.text] = 0.0;
-                }
-                aggregated.at(r.text) += p.second * r.score * r.score;
-            }
+        // search from N-gram index
+        std::vector<string_type> simstring_result;
+        {
+            std::lock_guard<std::mutex> lock(mutex_simstring);
+            db.retrieve(search_query, simstring_measure, simstring_threshold, std::back_inserter(simstring_result));
+        }
+        if(simstring_result.empty()){
+            return {};
+        }
+        else if(simstring_result.size() > max_candidate){
+            Eliminator<string_type> eliminate(search_query);
+            eliminate(simstring_result, max_candidate, true);
         }
 
-        return eval(aggregated, threshold, max_response);
+        return eval(query, simstring_result, threshold, max_response);
     }
 
-    std::vector<output_type> eval(const string_type& query, const std::vector<string_type>& targets,
+    std::vector<output_type> eval(const string_type& query, const std::vector<string_type>& candidates,
             double threshold = 0.0, size_t max_response = 0) const
     {
-        double t = max_candidate > 0 ? 0.0 : threshold;
-        size_t n = max_candidate > 0 ? max_candidate : max_response;
-
         // calculate similarity using all measures
         std::unordered_map<string_type, double> aggregated;
         for(auto p: resemblas){
-            for(auto r: p.first->eval(query, targets, t, n)){
+            for(auto r: p.first->eval(query, targets, 0.0, 0)){
                 if(aggregated.find(r.text) == std::end(aggregated)){
                     aggregated[r.text] = 0.0;
                 }
@@ -81,23 +90,6 @@ public:
             }
         }
 
-        return eval(aggregated, threshold, max_response);
-    }
-
-protected:
-    // name to be used in response
-    const std::string measure_name;
-
-    const size_t max_candidate;
-
-    double total_weight;
-
-    // pairs of Resembla and its weight
-    std::vector<std::pair<std::shared_ptr<ResemblaInterface>, double>> resemblas;
-
-    std::vector<output_type> eval(const std::unordered_map<string_type, double>& aggregated,
-            double threshold, size_t max_response) const;
-    {
         // sort combined result
         std::vector<output_type> response;
         for(auto r: aggregated){
@@ -114,6 +106,23 @@ protected:
         }
         return response;
     }
+
+protected:
+    struct Child
+    {
+        std::string name;
+        std::shared_ptr<ResemblaInterface> resembla;
+        double weight;
+    };
+
+    // name to be used in response
+    const std::string measure_name;
+
+    const size_t max_candidate;
+
+    double total_weight;
+
+    std::vector<Child> children;
 };
 
 }
