b75cb5d9f6741a7c074239a73a3f5c8aec3ce15a
tuem <uemura.takashi@gmail.com>
2017-09-29 10:15:01 +0900

    support comment line

--- src/string_util.hpp
+++ src/string_util.hpp
@@ -110,6 +110,21 @@ constexpr wchar_t value_delimiter()
     return L',';
 }
 
+template<typename char_type = char>
+constexpr char_type comment_prefix();
+
+template<>
+constexpr char comment_prefix()
+{
+    return '#';
+}
+
+template<>
+constexpr wchar_t comment_prefix()
+{
+    return L'#';
+}
+
 // split text by delimiter
 template<typename string_type>
 std::vector<string_type> split(const string_type& text, const typename string_type::value_type delimiter)
