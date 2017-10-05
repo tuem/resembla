4c1bd9f8b2fd3274e915c625f1af3e09f57fce03
tuem <uemura.takashi@gmail.com>
2017-10-05 17:25:41 +0900

    rename test functions

--- test/test_pronunciation_preprocessor.cpp
+++ test/test_pronunciation_preprocessor.cpp
@@ -32,7 +32,7 @@ const std::string PRONUNCIATION_SEQUENCE_PARSER_MECAB_OPTIONS = "-d ../misc/meca
 const size_t PRONUNCIATION_SEQUENCE_PARSER_MECAB_FUTURE_POS = 9;
 const std::string PRONUNCIATION_SEQUENCE_PARSER_MECAB_PRONUNCIATION_OF_MARKS = "記号";
 
-void test_pronunciation_sequence_builder_build(const std::wstring& input, const std::wstring& correct)
+void test_pronunciation_preprocessor(const std::wstring& input, const std::wstring& correct)
 {
     init_locale();
     PronunciationPreprocessor preprocess(
@@ -47,116 +47,104 @@ void test_pronunciation_sequence_builder_build(const std::wstring& input, const 
     CHECK(answer == correct);
 }
 
-void test_pronunciation_sequence_builder_build_indexing_text(const std::wstring& input, const std::wstring& correct)
-{
-    init_locale();
-    PronunciationPreprocessor preprocess(PRONUNCIATION_SEQUENCE_PARSER_MECAB_OPTIONS, PRONUNCIATION_SEQUENCE_PARSER_MECAB_FUTURE_POS, PRONUNCIATION_SEQUENCE_PARSER_MECAB_PRONUNCIATION_OF_MARKS);
-    auto answer = preprocess(input);
-#ifdef DEBUG
-    std::wcerr << "input text: " << input <<  std::endl;
-    std::wcerr << "indexing text: " << answer <<  std::endl;
-#endif
-    CHECK(answer == correct);
-}
-
 TEST_CASE( "parse an empty string to pronunciation sequence", "[language]" ) {
     std::wstring input = L"";
     std::wstring correct = L"";
-    test_pronunciation_sequence_builder_build(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a katakana word to pronunciation sequence", "[language]" ) {
     std::wstring input = L"シケン";
     std::wstring correct = L"シケン";
-    test_pronunciation_sequence_builder_build(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a hiragana word to pronunciation sequence", "[language]" ) {
     std::wstring input= L"しけん";
     std::wstring correct = L"シケン";
-    test_pronunciation_sequence_builder_build(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a kanji word to pronunciation sequence", "[language]" ) {
     std::wstring input = L"試験";
     std::wstring correct = L"シケン";
-    test_pronunciation_sequence_builder_build(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a sentence including hiragana, katakana and kanji to pronunciation sequence", "[language]" ) {
     std::wstring input = L"このテストは難関です";
     std::wstring correct = L"コノテストハナンカンデス";
-    test_pronunciation_sequence_builder_build(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a sentence including hiragana, katakana, kanji and unknown hiragana word to pronunciation sequence", "[language]" ) {
     std::wstring input = L"平仮名の未知語ぁぃぅぇぉをカタカナに変換する";
     std::wstring correct = L"ヒラガナノミチゴァィゥェォヲカタカナニヘンカンスル";
-    test_pronunciation_sequence_builder_build(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a sentence including hiragana, katakana, kanji and unknown katakana word to pronunciation sequence", "[language]" ) {
     std::wstring input = L"カタカナの未知語リセンブラをカタカナに変換する";
     std::wstring correct = L"カタカナノミチゴリセンブラヲカタカナニヘンカンスル";
-    test_pronunciation_sequence_builder_build(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a sentence including hiragana, katakana, kanji and unknown kanji word to pronunciation sequence", "[language]" ) {
     std::wstring input = L"漢字の未知語挫宛はそのまま出力される";
     std::wstring correct = L"カンジノミチゴ挫宛ハソノママシュツリョクサレル";
-    test_pronunciation_sequence_builder_build(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a sentence including old hiraganas", "[language]" ) {
     std::wstring input = L"ゐゑゔゕゖゝゞゟヰヱヿ";
     std::wstring correct = L"イエヴヵヶヽヾヨリイエコト";
-    test_pronunciation_sequence_builder_build(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a sentence including a conjugated form", "[language]" ) {
     std::wstring input = L"今日はもう出かけなきゃ";
     std::wstring correct = L"キョーハモウデカケナキャ";
-    test_pronunciation_sequence_builder_build(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a sentence including hiragana, katakana and kanji to text", "[language]" ) {
     std::wstring input = L"このテストは難関です";
     std::wstring correct = L"コノテストハナンカンデス";
-    test_pronunciation_sequence_builder_build_indexing_text(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a sentence including hiragana, katakana, kanji and unknown hiragana word to text", "[language]" ) {
     std::wstring input = L"平仮名の未知語ぁぃぅぇぉをカタカナに変換する";
     std::wstring correct = L"ヒラガナノミチゴァィゥェォヲカタカナニヘンカンスル";
-    test_pronunciation_sequence_builder_build_indexing_text(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a sentence including hiragana, katakana, kanji and unknown katakana word to text", "[language]" ) {
     std::wstring input = L"カタカナの未知語リセンブラをカタカナに変換する";
     std::wstring correct = L"カタカナノミチゴリセンブラヲカタカナニヘンカンスル";
-    test_pronunciation_sequence_builder_build_indexing_text(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a sentence including hiragana, katakana, kanji and unknown kanji word to text", "[language]" ) {
     std::wstring input = L"漢字の未知語挫宛はそのまま出力される";
     std::wstring correct = L"カンジノミチゴ挫宛ハソノママシュツリョクサレル";
-    test_pronunciation_sequence_builder_build_indexing_text(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a sentence including old hiraganas to text", "[language]" ) {
     std::wstring input = L"ゐゑゔゕゖゝゞゟヰヱヿ";
     std::wstring correct = L"イエヴヵヶヽヾヨリイエコト";
-    test_pronunciation_sequence_builder_build_indexing_text(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a sentence including a conjugated form to text", "[language]" ) {
     std::wstring input = L"今日はもう出かけなきゃ";
     std::wstring correct = L"キョーハモウデカケナキャ";
-    test_pronunciation_sequence_builder_build_indexing_text(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
 
 TEST_CASE( "parse a sentence including marks to text", "[language]" ) {
     std::wstring input = L"あっ!!今日何の日だっけ？教えて。";
     std::wstring correct = L"アッ!!キョーナンノヒダッケ？オシエテ。";
-    test_pronunciation_sequence_builder_build_indexing_text(input, correct);
+    test_pronunciation_preprocessor(input, correct);
 }
