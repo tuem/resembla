952aa9562184ff4a30d9b46507aceb06dca76c0d
tuem <uemura.takashi@gmail.com>
2017-11-26 20:42:31 +0900

    implement word vectors loader

--- src/word_vector_dictionary.hpp
+++ src/word_vector_dictionary.hpp
@@ -1,6 +1,4 @@
-/*
-Resembla
-https://github.com/tuem/resembla
+/* Resembla https://github.com/tuem/resembla
 
 Copyright 2017 Takashi Uemura
 
@@ -34,7 +32,29 @@ class WordVectorDictionary
 public:
     WordVectorDictionary(const std::string& dictionary_path)
     {
-        // TODO: load dictionary
+        std::ifstream ifs(dictionary_path);
+        if(ifs.fail()){
+            throw std::runtime_error("input file is not available: " + dictionary_path);
+        }
+
+        id_type current_id = 0;
+        while(true){
+            std::string line;
+            std::getline(ifs, line);
+            if(ifs.eof()){
+                break;
+            }
+
+            raw_values = split(line, ' ');
+            string_type surface = cast_string<string_type>(raw_values[0]);
+            std::vector<value_type> values;
+            for(size_t i = 1; i < raw_values.size(); ++i){
+                values.push_back(static_cast<value_type>(std::stod(raw_values[i])));
+            }
+
+            dictionary[current_id] = {current_id, surface, values};
+            ++current_id;
+        }
     }
 
     id_type id(const string_type& surface)
